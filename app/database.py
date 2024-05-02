7f5b6b79772507a2014db92663dfa6c07f669f3e
Timmy-Edibo <timothyedibo@gmail.com>
2024-05-02 14:48:32 +0100

    modified db file

--- app/database.py
+++ app/database.py
@@ -6,15 +6,15 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./learncha.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./learncha.db"
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
